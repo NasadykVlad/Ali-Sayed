e581f8e42f55ace7dc24ca355f94fc5bd1478ea6
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2021-10-20 20:13:01 +0300

    add-second-block-in-main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,12 @@
 "use strict"
 
 const burgerIcon = document.querySelector('.top-right_container--burger_icon');
+const btnViewHistory = document.querySelector('.button-view');
 
 burgerIcon.addEventListener("mouseout", function() {
+    this.style.transition = ".5s";
+});
+
+btnViewHistory.addEventListener("mouseout", function() {
     this.style.transition = ".5s";
 });
