972cba0cafcf7bade80bf08c31cbe30726f9ea1d
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2021-10-20 20:17:50 +0300

    change-script-js-and-add-class-to-button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,16 @@
 
 const burgerIcon = document.querySelector('.top-right_container--burger_icon');
 const btnViewHistory = document.querySelector('.button-view');
+const btnGo = document.querySelector('.button-go');
 
 burgerIcon.addEventListener("mouseout", function() {
     this.style.transition = ".5s";
 });
 
 btnViewHistory.addEventListener("mouseout", function() {
+    this.style.transition = ".5s";
+});
+
+btnGo.addEventListener("mouseout", function() {
     this.style.transition = ".5s";
 });
