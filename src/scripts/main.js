3d0f965f653be2046410696b1f77055fc66fae38
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2021-10-21 15:51:39 +0300

    finish-layout-and-check-on-validator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
-"use strict"
+"use strict";
 
 const burgerIcon = document.querySelector('.top-right_container--burger_icon');
 const btnViewHistory = document.querySelector('.button-view');
 const btnGo = document.querySelector('.button-go');
+const iconFacebook = document.querySelector('.icon-facebook');
+const iconTwitter = document.querySelector('.icon-twitter');
+const iconDribble = document.querySelector('.icon-dribble');
+const logo = document.querySelector('#logo');
 
 burgerIcon.addEventListener("mouseout", function() {
     this.style.transition = ".5s";
@@ -14,4 +18,26 @@ btnViewHistory.addEventListener("mouseout", function() {
 
 btnGo.addEventListener("mouseout", function() {
     this.style.transition = ".5s";
+});
+
+iconFacebook.addEventListener("mouseout", function() {
+    this.style.transition = ".5s";
+});
+
+iconTwitter.addEventListener("mouseout", function() {
+    this.style.transition = ".5s";
+});
+
+iconDribble.addEventListener("mouseout", function() {
+    this.style.transition = ".5s";
+});
+
+logo.addEventListener("mouseover", function() {
+    this.style.transition = "1s";
+    this.style.transform = "scale(1.5) rotate3d(1, 1, 1, 359deg)";
+});
+
+logo.addEventListener("mouseout", function() {
+    this.style.transition = "1s";
+    this.style.transform = "scale(1)";
 });
